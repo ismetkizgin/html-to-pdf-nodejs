be1827859b99e0efc6b39f73a01ae70dbec492df
ismet kizgin <info@ismetkizgin.com.tr>
2022-06-19 21:55:26 +0300

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+require("dotenv/config");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+app.listen(PORT, () => {
+  console.log("Ready on http://localhost:" + PORT);
+});
