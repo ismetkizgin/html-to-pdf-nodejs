65685309fadac1ade99832b9df9ad35c27b56770
ismet kizgin <info@ismetkizgin.com.tr>
2022-06-20 01:32:40 +0300

    feat: add web page to pdf example

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv/config");
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const puppeteer = require("puppeteer");
 
 const PORT = process.env.PORT || 5000;
 const app = express();
@@ -9,6 +10,31 @@ const app = express();
 app.use(cors());
 
 app.use(bodyParser.json());
+
+app.get("/pdf", async (req, res) => {
+  const url = process.env.WEBSITE_URL;
+
+  const browser = await puppeteer.launch({
+    headless: true,
+  });
+
+  const webPage = await browser.newPage();
+
+  await webPage.goto(url, {
+    waitUntil: "networkidle0",
+  });
+
+  await webPage.emulateMediaType("screen");
+
+  const pdf = await webPage.pdf({
+    printBackground: true,
+  });
+
+  await browser.close();
+
+  res.contentType("application/pdf");
+  res.send(pdf);
+});
 
 app.listen(PORT, () => {
   console.log("Ready on http://localhost:" + PORT);
